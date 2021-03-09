b6266faeb956e7f663f8950b316978e61e86c6ce
keemtj <keemgreat@gmail.com>
2021-03-09 20:04:58 +0900

    :sparkles: Add instagram data

--- src/Component/MyImages.js
+++ src/Component/MyImages.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const MyImages = () => {
-  const datas = [];
+  const [datas, setDatas] = useState(null);
+  console.log(datas);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        // eslint-disable-next-line no-undef
+        `https://graph.instagram.com/me/media?fields=id,caption,media_url,username&access_token=${process.env.REACT_APP_FACEBOOK_ACCESS_TOKEN}`,
+      );
+      const { data } = await response.data;
+      setDatas(data);
+    };
+    fetchData();
+  }, []);
+
+  if (!datas) return <div>loading...</div>;
   return (
     <StMyImagesWrapper>
       {datas?.map(({ id, media_url, caption }) => (
