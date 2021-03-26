b485ce080a1acab287a5b436ab1486f6f5702c6c
keemtj <keemgreat@gmail.com>
2021-03-26 14:34:59 +0900

    :sparkles: Add list to recommend users who have followed me

--- src/Component/Main/Aside.js
+++ src/Component/Main/Aside.js
@@ -14,40 +14,47 @@ const Aside = ({ setSignin }) => {
     }
   };
 
+  const followedMe = [
+    'username2',
+    'username3',
+    'username4',
+    'username5',
+    'username6',
+    'username7',
+    'username8',
+  ];
+
   return (
     <StAside>
       <StProfileImageWrapper>
         <ProfileImage
           src="/images/default_profile.png"
-          width={5}
-          height={5}
+          width={5.5}
+          height={5.5}
           username={'username'}
         />
-        <button onClick={handleLogout}>로그아웃</button>
+        <StButton onClick={handleLogout}>로그아웃</StButton>
       </StProfileImageWrapper>
-      <div>
-        <img />
-        <div>followers username</div>
-        <button>팔로우</button>
-        <img />
-        <div>followers username</div>
-        <button>팔로우</button>
-        <img />
-        <div>followers username</div>
-        <button>팔로우</button>
-        <img />
-        <div>followers username</div>
-        <button>팔로우</button>
-        <img />
-        <div>followers username</div>
-        <button>팔로우</button>
-      </div>
+      <StRecommendTitle>회원님을 위한 추천</StRecommendTitle>
+      <StRecommend>
+        {followedMe.slice(0, 5).map((username, index) => (
+          <StProfileImageWrapper key={index}>
+            <ProfileImage
+              username={username}
+              width={3.5}
+              height={3.5}
+              src="/images/default_profile.png"
+            />
+            <StButton>팔로우</StButton>
+          </StProfileImageWrapper>
+        ))}
+      </StRecommend>
     </StAside>
   );
 };
 
 const StAside = styled.aside`
-  border: 1px solid orange;
+  padding: 1.5rem 0rem;
 `;
 
 const StProfileImageWrapper = styled.div`
@@ -55,6 +62,27 @@ const StProfileImageWrapper = styled.div`
   flex-flow: row nowrap;
   align-items: center;
   justify-content: space-between;
+  & + & {
+    margin-top: 0.8rem;
+  }
+`;
+
+const StButton = styled.button`
+  color: #0092f5;
+  font-size: 1.2rem;
+  font-weight: 700;
+  cursor: pointer;
+`;
+
+const StRecommendTitle = styled.div`
+  margin-top: 1.5rem;
+  color: #828282;
+  font-size: 1.35rem;
+  font-weight: 600;
+`;
+
+const StRecommend = styled.div`
+  margin-top: 1rem;
 `;
 
 export default Aside;
