284e5eb1fd5263c99cb40d018feb4832e3b047dd
keemtj <keemgreat@gmail.com>
2021-06-14 16:13:50 +0900

    :fire: Remove post modal code

--- src/Component/Profile/PostModal.js
+++ src/Component/Profile/PostModal.js
@@ -4,27 +4,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { ChevronLeft } from '@styled-icons/entypo/ChevronLeft';
 import { ChevronRight } from '@styled-icons/entypo/ChevronRight';
 import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline';
-import { Heart } from '@styled-icons/bootstrap/Heart';
-import { PaperPlane } from '@styled-icons/ionicons-outline/PaperPlane';
-import { Chat } from '@styled-icons/bootstrap/Chat';
-import { Bookmark } from '@styled-icons/bootstrap/Bookmark';
-import { EmojiSmile } from '@styled-icons/bootstrap/EmojiSmile';
 import { activeIndex, activePostIdData, closePopup } from '../../Modules/popup';
 import PostPortal from '../../PostPortal';
-import Carousel from '../Global/Carousel';
-import ProfileImage from '../Global/ProfileImage';
-import { calcTimeElapsed } from '../../lib/calcTimeElapsed';
 import { useHistory } from 'react-router';
 
-const icons = [
-  { icon: <Heart /> },
-  { icon: <Chat /> },
-  { icon: <PaperPlane /> },
-  { icon: <Bookmark /> },
-];
-
 const PostModal = ({ posts, id, index }) => {
-  console.log(index);
   const modalRef = useRef();
   const dispatch = useDispatch();
   const his = useHistory();
@@ -46,28 +30,6 @@ const PostModal = ({ posts, id, index }) => {
   const onCloseButton = () => {
     dispatch(closePopup('postModal'));
   };
-
-  const comments = [
-    {
-      id: 'captain',
-      comment: 'hihihihihihihihihihihihihihihihihihi',
-      date: '2021-3-12',
-    },
-    { id: 'widow', comment: 'hihihihihihihihihihi', date: '2021-3-15' },
-    {
-      id: 'rocky',
-      comment: 'hihihihihihihihihihihihihihihihihihihihihihihihihihi',
-      date: '2021-3-20',
-    },
-    { id: 'ironman', comment: 'hihihihihihihihihihihihi', date: '2021-4-10' },
-    {
-      id: 'spiderman',
-      comment: 'hihihihihihihihihihihihihihihihi',
-      date: '2021-5-10',
-    },
-    { id: 'marble', comment: 'hihihihihihihihi', date: '2021-5-11' },
-    { id: 'dr_strange', comment: 'hihihihihihi', date: '2021-5-16' },
-  ];
 
   useEffect(() => {
     const path = his.location.pathname;
@@ -98,145 +60,29 @@ const PostModal = ({ posts, id, index }) => {
     <PostPortal>
       <StPostModalWrapper>
         <StPostBoxBlock ref={modalRef}>
-          <StImagesSection>
-            <Carousel imagesArray={posts[index]?.imagesArray} pagenation pos />
-          </StImagesSection>
-          <StPostSubDataSection>
-            <StHeader>
-              <ProfileImage
-                src={'/images/default_profile.png'}
-                alt={'temp'}
-                width={3.5}
-                height={3.5}
-              >
-                <div>
-                  <StDisplayName>{'displayName'}</StDisplayName>
-                  {posts[index]?.location && (
-                    <StLocation>{posts[index]?.location}</StLocation>
-                  )}
-                </div>
-              </ProfileImage>
-            </StHeader>
-            <StTextBox>
-              <StText>
-                {/* {post?.text} */}
-                {`⭐ | When your Setup looks so clean and
-                neat, your day will become more inspired and creative. Don't you
-                agree?
-                ➖➖➖➖➖➖➖➖➖➖➖➖
-                Follow for more amazing content
-                | Follow Us | 👉 @hermeshitech
-                | Follow Us | 👉 @hermeshitech
-                | Follow Us | 👉 @hermeshitech 
-                ➖➖➖➖➖➖➖➖➖➖➖➖⠀ 
-                📸 | Credits: @nvzion 
-                🏅 | Tag a Friend down below⠀ 
-                👇 | Comment Your Thoughts here
-                ➖➖➖➖➖➖➖➖➖➖➖➖⠀
-                #hermeshitech
-                #techlifestyle #desksetups #setupinspiration
-                #workspaceinspiration #productivespaces #myworkspace
-                #designyourworkspace #minimalismsource #workspacedesign
-                #minimalsetups #beautyoftechnology #designoffice #deskgoals
-                #officeinspiration #techsetup #dreamdesks #woodendesktop
-                #macbookpro #setup #pcsetup
-                `}
-              </StText>
-              <StCommentsBox>
-                {/* {post?.comments?.map((comment, index) => ( */}
-                {comments?.map((comment, index) => (
-                  <div
-                    key={index}
-                    style={{
-                      marginTop: '2rem',
-                      display: 'flex',
-                    }}
-                  >
-                    {/* <div key={index}>
-                    <StUsername>{comment.id}</StUsername>{' '}
-                    <span>{comment.comment}</span>
-                  </div> */}
-                    <div>
-                      <img
-                        src={'/images/default_profile.png'}
-                        alt={'temp'}
-                        style={{
-                          borderRadius: '50%',
-                          width: '3.5rem',
-                          height: '3.5rem',
-                          objectFit: 'cover',
-                        }}
-                      />
-                    </div>
-                    <div
-                      style={{
-                        display: 'flex',
-                        flexFlow: 'column',
-                      }}
-                    >
-                      <div></div>
-                      <div
-                        style={{
-                          marginLeft: '1.5rem',
-                          lineHeight: '1.4',
-                          height: 'fit-content',
-                        }}
-                      >
-                        <StUsername>{comment.id}</StUsername>{' '}
-                        <span>{comment.comment}</span>
-                      </div>
-                      <div
-                        style={{
-                          paddingLeft: '1.5rem',
-                          marginTop: '0.5rem',
-                          fontSize: '1.1rem',
-                          color: 'gray',
-                        }}
-                      >
-                        {calcTimeElapsed(comment.date)}
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </StCommentsBox>
-            </StTextBox>
-            <StSectionNav>
-              {icons.map((icon, index) => (
-                <span key={index}>{icon.icon}</span>
-              ))}
-            </StSectionNav>
-            {posts[index]?.heartCount > 0 ? (
-              <StHeartCount>좋아요 {posts[index]?.heartCount}개</StHeartCount>
-            ) : (
-              <StHeartCount>가장 먼저 좋아요를 눌러주세요</StHeartCount>
-            )}
-            <StDate>{calcTimeElapsed(posts[index]?.date)}</StDate>
-            {!posts[index]?.isPossibleComment && (
-              <StChatCommentLabel>
-                <button>
-                  <StEmojiSmile />
-                </button>
-                <StCommentInput type="text" placeholder="댓글 달기..." />
-                <StCommentButton type="submit">게시</StCommentButton>
-              </StChatCommentLabel>
-            )}
-          </StPostSubDataSection>
-          <StSlideButton
-            type="button"
-            role="prev"
-            onClick={handlePrev}
-            hidden={index === 0 ? true : false}
-          >
-            <StPrevButton />
-          </StSlideButton>
-          <StSlideButton
-            type="button"
-            role="next"
-            onClick={handleNext}
-            hidden={index === lastIndex ? true : false}
-          >
-            <StNextButton />
-          </StSlideButton>
+          {posts.map((post, index) => {
+            console.log(post);
+            return (
+              <StPostBoxBlockInner key={index}>
+                <StSlideButton
+                  type="button"
+                  role="prev"
+                  onClick={handlePrev}
+                  hidden={index === 0 ? true : false}
+                >
+                  <StPrevButton />
+                </StSlideButton>
+                <StSlideButton
+                  type="button"
+                  role="next"
+                  onClick={handleNext}
+                  hidden={index === lastIndex ? true : false}
+                >
+                  <StNextButton />
+                </StSlideButton>
+              </StPostBoxBlockInner>
+            );
+          })}
         </StPostBoxBlock>
         <StCloseButton type="button" onClick={onCloseButton}>
           <CloseOutline />
@@ -262,139 +108,14 @@ const StPostBoxBlock = styled.main`
   display: flex;
   flex-flow: row nowrap;
   width: 95rem;
+  min-width: 95rem;
   height: 59.9rem;
   background: ${({ theme }) => theme.white};
+  overflow: hidden;
 `;
 
-const StImagesSection = styled.section`
+const StPostBoxBlockInner = styled.div`
   display: flex;
-  align-items: center;
-  border-right: 1px solid ${({ theme }) => theme.gray};
-  width: 100%;
-  max-width: 60rem;
-`;
-
-const StPostSubDataSection = styled.section`
-  display: flex;
-  flex-flow: column nowrap;
-  align-items: flex-start;
-  width: 35rem;
-`;
-
-const StHeader = styled.header`
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid ${({ theme }) => theme.gray};
-  padding: 1.5rem;
-  width: 100%;
-  & > div {
-    display: flex;
-    align-items: center;
-    & > div {
-      margin-left: 1.5rem;
-    }
-  }
-  font-size: 1.4rem;
-  font-weight: 600;
-`;
-
-const StDisplayName = styled.h2`
-  &:hover {
-    text-decoration: underline;
-    cursor: pointer;
-  }
-`;
-
-const StLocation = styled.div`
-  margin-top: 0.2rem;
-  font-size: 1.2rem;
-  font-weight: 400;
-  color: ${({ theme }) => theme.darkGray};
-`;
-
-const StTextBox = styled.div`
-  display: flex;
-  flex-flow: column nowrap;
-  align-items: flex-start;
-  height: 100%;
-  padding: 1.5rem;
-  overflow: scroll;
-  font-size: 1.4rem;
-  word-break: break-all;
-`;
-
-const StText = styled.div`
-  width: 100%;
-  line-height: 1.3;
-`;
-
-const StUsername = styled.span`
-  font-weight: 600;
-`;
-
-const StCommentsBox = styled.article`
-  width: 100%;
-  font-size: 1.4rem;
-  & > div {
-    margin-top: 0.5rem;
-  }
-`;
-
-const StSectionNav = styled.article`
-  display: flex;
-  align-items: center;
-  width: 100%;
-  padding: 1.5rem;
-  & > span {
-    width: 2.5rem;
-    height: 2.5rem;
-  }
-  & > span + span {
-    margin-left: 1.5rem;
-  }
-  & > :last-child {
-    margin-left: auto;
-  }
-  border-top: 1px solid ${({ theme }) => theme.gray};
-`;
-
-const StHeartCount = styled.div`
-  width: 100%;
-  padding: 0rem 1.5rem;
-  font-size: 1.4rem;
-  font-weight: 600;
-`;
-
-const StDate = styled.div`
-  padding: 1.5rem;
-  color: ${({ theme }) => theme.darkGray};
-`;
-
-const StChatCommentLabel = styled.label`
-  border-top: 1px solid ${({ theme }) => theme.gray};
-  padding: 1.5rem;
-  width: 100%;
-  display: flex;
-  align-items: center;
-`;
-
-const StEmojiSmile = styled(EmojiSmile)`
-  width: 3rem;
-`;
-
-const StCommentInput = styled.input`
-  border: none;
-  background: ${({ theme }) => theme.white};
-  margin-left: 1.5rem;
-  width: 100%;
-  outline: none;
-`;
-
-const StCommentButton = styled.button`
-  width: 4rem;
-  font-size: 1.5rem;
-  font-weight: 600;
-  color: ${({ theme }) => theme.activeBlue};
 `;
 
 const buttonStyle = css`
