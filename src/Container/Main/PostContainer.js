29c8d20e356aafb5d1ff52ba8ef9e97bbe7035da
keemtj <keemgreat@gmail.com>
2021-06-17 19:43:14 +0900

    :recycle: Refactor calcTimeElapsed function

--- src/Container/Main/PostContainer.js
+++ src/Container/Main/PostContainer.js
@@ -25,25 +25,7 @@ import {
 import { getHearts, getUsersWhoClickHeart } from '../../Modules/heart';
 import { getBookmarks } from '../../Modules/saved';
 import { activePostData, openPopup } from '../../Modules/popup';
-
-// NOTE 경과 시간 계산 함수
-const calcTimeElapsed = date => {
-  const start = new Date(date);
-  const end = Date.now();
-  const sec = Math.floor((end - start) / 1000); // 경과시간, 초
-  const min = Math.floor((end - start) / 1000 / 60); // 경과시간, 분
-  const hour = Math.floor((end - start) / 1000 / 60 / 60); // 경과시간, 시간
-  const day = Math.floor((end - start) / 1000 / 60 / 60 / 24); // 경과시간, 일
-  const elapsed =
-    sec >= 60
-      ? min >= 60
-        ? hour >= 24
-          ? day + '일전'
-          : hour + '시간 전'
-        : min + '분 전'
-      : '방금 전';
-  return elapsed;
-};
+import { calcTimeElapsed } from '../../lib/calcTimeElapsed';
 
 const PostContainer = ({
   post,
